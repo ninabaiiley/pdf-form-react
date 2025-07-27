9bac3303eff874c5bdae9286fbd016b0550f58ad
ninabaiiley <ninabaiiley1302@gmail.com>
2025-07-27 07:33:38 -0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="form-container">
+      <h2>Enter email to download files</h2>
+      <input type="email" placeholder="Enter your email" />
+      <button>Download</button>
+    </div>
+  );
+}
+
+export default App;
